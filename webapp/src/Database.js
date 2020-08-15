be37cf4312be57304c2c5f625e42d77c5219fc20
Abhay D <a.deshpande012@gmail.com>
2020-08-15 15:35:01 -0700

    Added database code to pull geohash data

---
+++ webapp/src/Database.js
@@ -0,0 +1,50 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import Geohash from "latlon-geohash";
+
+const config = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
+
+export default class Database {
+    constructor() {
+        firebase.initializeApp(config);
+        this.firestore = firebase.firestore();
+        this.users = this.firestore.collection("users");
+
+        this.addUser = this.addUser.bind(this);
+        this.getUsersInArea = this.getUsersInArea.bind(this);
+    }
+
+    addUser(name, lat, long) {
+        const hash = Geohash.encode(lat, long);
+        this.users.doc(name).set({
+            location: hash,
+            lat: lat,
+            long: long,
+            timestamp: Date.now()
+        });
+    }
+
+    getUsersInArea(lat, long, precision, callback) {
+        const start = Geohash.encode(lat, long, precision);
+        const end = start.replace(/.$/, c => String.fromCharCode(c.charCodeAt(0) + 1));
+        this.users
+            .where("location", ">=", start)
+            .where("location", "<", end)
+            .get()
+            .then(function (q) {
+                callback(q.docs.map(doc => {
+                    const data = doc.data();
+                    return {lat: data.lat, lng: data.long};
+                }));
+            });
+    }
+}
