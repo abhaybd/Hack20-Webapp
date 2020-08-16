d491b7b6e5608fecfb6be55c4999749d7993932b
x9du <zhou.cindy02@gmail.com>
2020-08-15 20:25:38 -0700

    Add search bar

--- webapp/src/Map.js
+++ webapp/src/Map.js
@@ -1,5 +1,6 @@
 import React from "react";
-import {GoogleMap, HeatmapLayer, LoadScript} from '@react-google-maps/api';
+import {GoogleMap, HeatmapLayer, LoadScript, Marker, StandaloneSearchBox} from '@react-google-maps/api';
+import Geocode from "react-geocode";
 import {db} from "./Database";
 
 const containerStyle = {
@@ -7,7 +8,9 @@ const containerStyle = {
     height: '100vh'
 };
 
-const libraries = ["visualization"];
+const libraries = ["visualization", "places"];
+
+Geocode.setApiKey("AIzaSyAb1opXdUUzPE2NzzKwbDVpDowXreTkPpo");
 
 function renderHeatmap(data, LatLng, bounds) {
     // Za = latitude, Va = longitude
@@ -22,6 +25,9 @@ function renderHeatmap(data, LatLng, bounds) {
 
 export default function Map(props) {
     const [map, setMap] = React.useState(null);
+    let currentLocation = {lat: 47.655548, lng: -122.303200};
+    let searchBox;
+    let marker;
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(getPosition);
@@ -31,6 +37,7 @@ export default function Map(props) {
         if (map) {
             map.setCenter({lat: position.coords.latitude, lng: position.coords.longitude});
         }
+        currentLocation = {lat: position.coords.latitude, lng: position.coords.longitude};
     }
 
     const onLoad = React.useCallback(function callback(map) {
@@ -52,6 +59,22 @@ export default function Map(props) {
         renderHeatmap(data, window.google.maps.LatLng, map.getBounds());
     }
 
+    const searchBoxLoad = ref => searchBox = ref;
+    const onPlacesChanged = () => Geocode.fromAddress(searchBox.getPlaces()[0]['formatted_address']).then(
+        response => {
+            marker.position = response.results[0].geometry.location;
+            if(map) {
+                map.setCenter(marker.position)
+            }
+            console.log(marker.position)
+        },
+        error => {
+            console.error(error);
+        }
+    );
+
+    const markerLoad = ref => marker = ref;
+
     return (
         <LoadScript
             googleMapsApiKey="AIzaSyAb1opXdUUzPE2NzzKwbDVpDowXreTkPpo"
@@ -65,6 +88,37 @@ export default function Map(props) {
                 onUnmount={onUnmount}
                 onIdle={onIdle}
             >
+                <StandaloneSearchBox
+                    onLoad={searchBoxLoad}
+                    onPlacesChanged={
+                        onPlacesChanged
+                    }
+                    >
+                    <input
+                        type="text"
+                        placeholder="Customized your placeholder"
+                        style={{
+                        boxSizing: `border-box`,
+                        border: `1px solid transparent`,
+                        width: `240px`,
+                        height: `32px`,
+                        padding: `0 12px`,
+                        borderRadius: `3px`,
+                        boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+                        fontSize: `14px`,
+                        outline: `none`,
+                        textOverflow: `ellipses`,
+                        position: "absolute",
+                        left: "50%",
+                        marginLeft: "-120px"
+                        }}
+                    />
+                </StandaloneSearchBox>
+                <Marker
+                    onLoad={markerLoad}
+                    onPositionChanged={markerLoad}
+                    position={currentLocation}
+                />
                 <HeatmapLayer data={[]} onLoad={heatmapLoad}/>
                 { /* Child components, such as markers, info windows, etc. */}
             </GoogleMap>
