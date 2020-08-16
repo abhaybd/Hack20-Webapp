3ca74827c62e26dc47fe2cd06c316b019a922dfd
Ian Wang <35878784+iwangy@users.noreply.github.com>
2020-08-15 19:24:13 -0700

    Put map into function App

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -11,25 +11,24 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { NavigationBar } from './NavigationBar';
 import Sidebar from './Sidebar';
-// const uw = {lat: 47.655548, lng: -122.303200};
-
-// const App = () => (
-//   <Map center={uw}/>
-// );
+const uw = {lat: 47.655548, lng: -122.303200};
 
 function App(){
   return(
-    <React.Fragment>
-      <Router>
-        <NavigationBar />
-        <Sidebar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route component={NoMatch} />
-        </Switch>
-      </Router>
-    </React.Fragment> 
+    <div>
+      <Map center = {uw}/>
+      <React.Fragment>
+        <Router>
+          <NavigationBar />
+          <Sidebar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route component={NoMatch} />
+          </Switch>
+        </Router>
+      </React.Fragment> 
+    </div>
   );
 }
 
