60f4474031aa7f327558c857947088abdc139b09
Ian Wang <35878784+iwangy@users.noreply.github.com>
2020-08-15 19:37:57 -0700

    Revert "Navigation and Sidebar"

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -2,33 +2,10 @@ import React from 'react';
 import './App.css';
 import Map from "./Map";
 
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import { Home } from './Home';
-import { About } from './About';
-import { NoMatch } from './NoMatch';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { NavigationBar } from './NavigationBar';
-import Sidebar from './Sidebar';
+let currentLocation = {lat: 47.655548, lng: -122.303200};
 
-let currentLocation = { lat: 47.655548, lng: -122.303200 };
-
-function App() {
-  return (
-    <div>
-      <React.Fragment>
-        <Router>
-          <NavigationBar />
-          <Sidebar />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route component={NoMatch} />
-          </Switch>
-        </Router>
-      </React.Fragment>
-      <Map center={currentLocation} />
-    </div>
-  );
-}
+const App = () => (
+  <Map center={currentLocation}/>
+);
 
 export default App;
